838f3daca127fc84e99dc1812f1564a69c9f7e6a
sumaar1981 <45775265+sumaar1981@users.noreply.github.com>
2018-12-23 07:19:01 +0530

    Add files via upload

---
+++ server.R
@@ -0,0 +1,87 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+options(shiny.maxRequestSize = 30*1024^2)
+
+shinyServer(function(input, output) {
+ 
+  textfile <- reactive({
+    textfile <- input$file1
+    if (is.null(textfile)) {
+     return()
+    }
+    return(readLines(textfile$datapath,encoding = 'UTF-8'))
+  })
+  
+  annotate_data = reactive({
+    dataset =  (textfile())
+    dataset  =  str_replace_all(dataset, "<.*?>", "") # get rid of html junk
+   
+    annotate_data <- input$file2
+    model = udpipe_load_model(annotate_data$datapath)
+    
+    # now annotate text dataset using ud_model above
+    # system.time({   # ~ depends on corpus size
+    x <- udpipe_annotate(model, x = dataset) #%>% as.data.frame() %>% head()
+    x <- as.data.frame(x)
+    x <- subset(x,select=-c(sentence))
+    return(x)
+  })
+  
+  output$plot = renderPlot({
+    x=annotate_data()
+    windowsFonts(devanew=windowsFont("Devanagari new normal"))
+    data_cooc <- cooccurrence(x = subset(x, upos %in% c(input$checkGroup)),
+                              term = "lemma",
+                              group = c("doc_id", "paragraph_id", "sentence_id"))
+    
+    wordnetwork <- head(data_cooc, input$coor)
+    wordnetwork <- igraph::graph_from_data_frame(wordnetwork) # needs edgelist in first 2 colms.
+    
+    ggraph(wordnetwork, layout = "fr") + 
+      geom_edge_link(aes(width = cooc, edge_alpha = cooc), edge_colour = "orange") +  
+      geom_node_text(aes(label = name), col = "darkgreen", size = 4) +
+      theme_graph(base_family = "Arial Narrow") +  
+      labs(title = "Cooccurrences at Sentence Level")
+    
+  })
+  
+  output$skipplot = renderPlot({
+    x=annotate_data()
+    windowsFonts(devanew=windowsFont("Devanagari new normal"))
+    data_cooc_skip <- cooccurrence(x$lemma, 
+                                   relevant = x$upos %in% c(input$checkGroup),
+                                   skipgram = input$skipgram)
+                               
+
+    wordnetwork <- head(data_cooc_skip, input$coor)
+    wordnetwork <- igraph::graph_from_data_frame(wordnetwork) # needs edgelist in first 2 colms.
+    
+    ggraph(wordnetwork, layout = "fr") + 
+      geom_edge_link(aes(width = cooc, edge_alpha = cooc), edge_colour = "orange") +  
+      geom_node_text(aes(label = name), col = "darkgreen", size = 4) +
+      theme_graph(base_family = "Arial Narrow") +  
+      labs(title = "Cooccurrences within skipgram words distance")
+    
+  })
+    output$wordcloud = renderPlot({
+    x=annotate_data()
+    all_nouns = x %>% subset(., upos %in% c(input$checkGroup))
+    top_nouns = txt_freq(all_nouns$lemma)
+    wordcloud(words = top_nouns$key, 
+              freq = top_nouns$freq, 
+              min.freq = input$freq, 
+              max.words = input$max,
+              random.order = FALSE, 
+              colors = brewer.pal(6, "Dark2"))
+    
+    })
+ 
+  
+
+})
---
+++ ui.R
@@ -0,0 +1,103 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+library(text2vec)
+library(tm)
+library(tokenizers)
+library(wordcloud)
+library(slam)
+library(stringi)
+library(magrittr)
+library(tidytext)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggraph)
+library(ggplot2)
+
+
+
+
+shinyUI(
+  fluidPage(
+    
+    titlePanel("Co-occurences plot"),
+    sidebarLayout(
+      sidebarPanel(  
+        
+        fileInput("file1", "Upload data (Text file)"),
+                    multiple = FALSE,
+                    accept = c("text/csv",
+                   "text/comma-separated-values,text/plain", ".csv"),
+        fileInput("file2", "Upload udpipe model"),
+                  multiple = FALSE,
+                  accept = c(".udpipe"),  
+        checkboxGroupInput("checkGroup", "XPOS", 
+                         c("Adjective (JJ)" = "ADJ", "Noun(NN)" = "NOUN", 
+                           "Proper Noun (NNP)" = "PROPN","Adverb (RB)" = "ADV", 
+                           "Verb (VB)" = "VERB"), selected = c("ADJ","NOUN","PROPN")),
+        sliderInput("freq", "Minimum Frequency in Wordcloud:", min = 0,  max = 100, value = 2),
+        
+        sliderInput("max",  "Maximum Number of Words in Wordcloud:", min = 1,  max = 300,  value = 50),
+        sliderInput("coor",  "Maximum Number of co-occurences:", min = 1,  max = 100,  value = 50),
+        sliderInput("skipgram",  "Maximum Number of skipgram words:", min = 1,  max = 10,  value = 4)
+        ),
+      
+     
+      mainPanel(
+        
+        tabsetPanel(type = "tabs",
+                    tabPanel("Overview",h4(p("Data input")),
+                             p("This app supports input as text file.",align="justify"),
+                             p("Please refer to the link below for sample input file."),
+                             a(href="https://github.com/sudhir-voleti/sample-data-sets/blob/master/Segmentation%20Discriminant%20and%20targeting%20data/ConneCtorPDASegmentation.csv"
+                               ,"Sample data input file"),   
+                             br(),br(),
+                             p("You also need to provide udpipe model as second input.",align="justify"),
+                             h4('How to use this App'),
+                             p('To use this app, click on', 
+                               span(strong("Upload data (Text file)")),
+                               'and upload data file. Also upload the udpipe model(English,Spanish or Hindi etc) as second input depending on text language.
+                               Once both the files are uploaded, app will do computations with default inputs and
+                               will display Co-occurance Plots and Word cloud in respective tabs.',
+                              br(),
+                               'Below options are given in left side panel to change inputs as per your requirement.',
+                              br(),
+                               '- XPOS to choose feature',
+                              br(),
+                               '- select minimum frequency of word to display in word cloud',
+                              br(),
+                               '- select maximum number of words to display in word cloud',
+                              br(),
+                               '- select maximum number of co-occurances  to display in both co-occurances plots',
+                              br(),
+                               '- select number of skipgrams to consider for co-occurance plot at corpus level. Skipgram 1 will
+                                  display words following each other at corpus level'
+                               )),
+                    
+                    tabPanel("Co-occurence plot", 
+                             plotOutput("plot"),
+                             br(),
+                             br(),
+                             plotOutput("skipplot")
+                             ),
+                    tabPanel("Word Cloud",  
+                             plotOutput("wordcloud",height = 700, width = 700))
+                    
+        ) # end of tabsetPanel  
+      )# end of main panel
+    ) # end of sidebarLayout
+  ) # end of fluidPage
+) # end of UI
+
+
